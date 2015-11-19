520528322da50a2e4358e1f66c21a93a5ace25a4
jessiRedux13 <nvrfade_me@yahoo.com>
2015-11-19 18:49:11 +0800

    keepOnDreaming

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Road to Dreams Never End");
+            Console.ReadKey();
         }
     }
 }
