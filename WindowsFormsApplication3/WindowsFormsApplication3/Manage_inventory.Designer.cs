8052ee674bcceee0ea29a384432b9f41b779be2c
jessiRedux13 <nvrfade_me@yahoo.com>
2015-12-07 22:28:19 +0800

    Second Try

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Manage_inventory.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Manage_inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtboxU_in = new System.Windows.Forms.TextBox();
+            this.txtboxU_out = new System.Windows.Forms.TextBox();
+            this.txtboxp_price = new System.Windows.Forms.TextBox();
+            this.cbopid_pname = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dgvmanage = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.Date_Added = new System.Windows.Forms.Label();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmanage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtboxU_in
+            // 
+            this.txtboxU_in.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxU_in.Location = new System.Drawing.Point(174, 128);
+            this.txtboxU_in.Name = "txtboxU_in";
+            this.txtboxU_in.Size = new System.Drawing.Size(199, 21);
+            this.txtboxU_in.TabIndex = 0;
+            // 
+            // txtboxU_out
+            // 
+            this.txtboxU_out.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxU_out.Location = new System.Drawing.Point(174, 174);
+            this.txtboxU_out.Name = "txtboxU_out";
+            this.txtboxU_out.Size = new System.Drawing.Size(199, 21);
+            this.txtboxU_out.TabIndex = 1;
+            // 
+            // txtboxp_price
+            // 
+            this.txtboxp_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxp_price.Location = new System.Drawing.Point(174, 218);
+            this.txtboxp_price.Name = "txtboxp_price";
+            this.txtboxp_price.Size = new System.Drawing.Size(199, 21);
+            this.txtboxp_price.TabIndex = 2;
+            // 
+            // cbopid_pname
+            // 
+            this.cbopid_pname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbopid_pname.FormattingEnabled = true;
+            this.cbopid_pname.Location = new System.Drawing.Point(174, 79);
+            this.cbopid_pname.Name = "cbopid_pname";
+            this.cbopid_pname.Size = new System.Drawing.Size(199, 23);
+            this.cbopid_pname.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(30, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(30, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Unit_in_Stock";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(30, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Unit_out_Stock";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(30, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Purchase_Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(30, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Expired_Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(173, 265);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(99, 21);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // dgvmanage
+            // 
+            this.dgvmanage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvmanage.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column2,
+            this.Column6,
+            this.Column7});
+            this.dgvmanage.Location = new System.Drawing.Point(469, 79);
+            this.dgvmanage.Name = "dgvmanage";
+            this.dgvmanage.Size = new System.Drawing.Size(740, 301);
+            this.dgvmanage.TabIndex = 11;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "PID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Unit_in_Stock";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Unit_out_Stock";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Purchase_Price";
+            this.Column5.Name = "Column5";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Expired_Date";
+            this.Column2.Name = "Column2";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Prod_Added_Date";
+            this.Column6.Name = "Column6";
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(173, 357);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(77, 23);
+            this.btnsave.TabIndex = 12;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(310, 357);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(77, 23);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(666, 48);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker2.TabIndex = 18;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // Date_Added
+            // 
+            this.Date_Added.AutoSize = true;
+            this.Date_Added.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Date_Added.Location = new System.Drawing.Point(575, 54);
+            this.Date_Added.Name = "Date_Added";
+            this.Date_Added.Size = new System.Drawing.Size(85, 15);
+            this.Date_Added.TabIndex = 19;
+            this.Date_Added.Text = "Date_Added";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Total_Stock";
+            this.Column7.Name = "Column7";
+            // 
+            // Manage_inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication3.Properties.Resources.abstract_crimson_landscape_backgrounds_powerpoint;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1211, 682);
+            this.Controls.Add(this.Date_Added);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.dgvmanage);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbopid_pname);
+            this.Controls.Add(this.txtboxp_price);
+            this.Controls.Add(this.txtboxU_out);
+            this.Controls.Add(this.txtboxU_in);
+            this.Name = "Manage_inventory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Manage_inventory";
+            this.Load += new System.EventHandler(this.Manage_inventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmanage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtboxU_in;
+        private System.Windows.Forms.TextBox txtboxU_out;
+        private System.Windows.Forms.TextBox txtboxp_price;
+        private System.Windows.Forms.ComboBox cbopid_pname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dgvmanage;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label Date_Added;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Manage_inventory.cs
@@ -0,0 +1,140 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Manage_inventory : Form
+    {
+        private MySqlConnection connection;
+        String ConnectionString = "Server=192.168.0.24;Database=inventory_food_meds;Uid=jes;Pwd=!ojt2015";
+        public Manage_inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void Product_id()
+        {
+            connection = new MySqlConnection(ConnectionString);
+
+            try
+            {
+                //open connection
+                connection.Open();
+
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand("select * from product_info", connection);
+
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                   cbopid_pname.Items.Add(dataReader["pid"] + "|" + dataReader["p_name"]);
+                  
+
+                }
+                //close connection
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Manage_inventory_Load(object sender, EventArgs e)
+        {
+            Product_id();
+        }
+
+        private void LoadAllproduct()
+        {
+            connection = new MySqlConnection(ConnectionString);
+            dgvmanage.Rows.Clear();
+            try
+            {
+                //open connection
+                connection.Open();
+               // string[] p_id = cbopid2.Text.Split(new char[] { '|' });
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand("select * from Stock_status where date(Prod_Added_Date)='"+dateTimePicker2.Value.ToString("yyyy-MM-dd")+"' ", connection);
+
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    dgvmanage.Rows.Add(dataReader["pid"] + "", dataReader["Unit_in_stock"] + "", dataReader["Unit_out_stock"] + "", dataReader["Purchase_price"] + "", Convert.ToDateTime(dataReader["expired_date"]).ToString("yyyy-MM-dd"), dataReader["Prod_Added_Date"] + "");
+                }
+                //close connection
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnload_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void insertStock_status()
+        {
+            connection = new MySqlConnection(ConnectionString);
+            try
+            {
+
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into Stock_status(pid,Unit_in_stock,Unit_out_stock,Purchase_Price,Expired_Date) value('"+cbopid_pname.Text+"','" + txtboxU_in.Text + "','" + txtboxU_out.Text + "','" + txtboxp_price.Text + "','" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + "')", connection);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Successfully saved");
+
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void Subtract()
+        {
+            
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            insertStock_status();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            LoadAllproduct();
+        }
+
+       
+        
+
+  
+
+       
+
+    }
+}
